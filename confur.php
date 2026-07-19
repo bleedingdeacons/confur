4c234d0a590c24f784de4ebbf241ab830276ef57
Dave <crazeydave@users.noreply.github.com>
2026-07-19 21:35:52 +0100

    Release 2.12.1

--- confur.php
+++ confur.php
@@ -3,7 +3,7 @@
  * Plugin Name: Confur
  * Plugin URI:
  * Description: Automated collation of answers to questions for conference.
- * Version: 2.12.0
+ * Version: 2.12.1
  * Requires at least: 6.0
  * Requires PHP: 8.1
  * GitHub Plugin URI: https://github.com/thebleedingdeacons/confur
